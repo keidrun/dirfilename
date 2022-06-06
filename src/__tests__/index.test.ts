3efedf6319dc396e0e8282486672259cc35f6cd4
keidrun <kssk96@gmail.com>
2022-06-06 23:32:18 +0900

    test: change test library to vitest from jest, and add error tests'

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,58 @@
+import { describe, test, expect } from 'vitest'
+
 import { filename, dirname } from '../index.js'
 
-describe('utils', () => {
+describe('index', () => {
   describe('filename', () => {
     test('should return a file path', () => {
       expect(filename({ url: 'file:///dir/file.txt' })).toBe('/dir/file.txt')
+    })
+    test('should throw an error if provided with empty url', () => {
+      const resultFn = () => {
+        filename({ url: '' })
+      }
+
+      expect(resultFn).toThrow(/Invalid URL/)
+    })
+    test('should throw an error if provided with undefined url', () => {
+      const resultFn = () => {
+        filename({ url: undefined })
+      }
+
+      expect(resultFn).toThrow(/The "path" argument must be of type string or an instance of URL/)
+    })
+    test('should throw an error if provided with null url', () => {
+      const resultFn = () => {
+        filename({ url: null })
+      }
+
+      expect(resultFn).toThrow(/The "path" argument must be of type string or an instance of URL/)
     })
   })
   describe('dirname', () => {
     test('should return a directory path', () => {
       expect(dirname({ url: 'file:///dir/file.txt' })).toBe('/dir')
+    })
+    test('should throw an error if provided with empty url', () => {
+      const resultFn = () => {
+        dirname({ url: '' })
+      }
+
+      expect(resultFn).toThrow(/Invalid URL/)
+    })
+    test('should throw an error if provided with undefined url', () => {
+      const resultFn = () => {
+        dirname({ url: undefined })
+      }
+
+      expect(resultFn).toThrow(/The "path" argument must be of type string or an instance of URL/)
+    })
+    test('should throw an error if provided with null url', () => {
+      const resultFn = () => {
+        dirname({ url: null })
+      }
+
+      expect(resultFn).toThrow(/The "path" argument must be of type string or an instance of URL/)
     })
   })
 })
